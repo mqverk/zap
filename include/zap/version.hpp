68c24a4d8c3082936fad43f25e6950a39a0c773d
mqverk <mqverk@users.noreply.github.com>
2026-03-10 23:13:45 +0530

    feat: add version constants header

---
+++ include/zap/version.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace zap {
+
+constexpr const char* VERSION       = "0.1.0";
+constexpr int         VERSION_MAJOR = 0;
+constexpr int         VERSION_MINOR = 1;
+constexpr int         VERSION_PATCH = 0;
+
+constexpr const char* DESCRIPTION   = "The zero-friction C++ package manager";
+constexpr const char* HOMEPAGE      = "https://github.com/zapbuild/zap";
+
+} // namespace zap
